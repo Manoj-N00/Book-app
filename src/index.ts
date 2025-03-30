ef597e487453d33501c28955c24a9f86cb354812
Manoj-N00 <manojnaikj@gmail.com>
2025-03-31 01:29:53 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ async function main() {
   try {
     await prisma.$connect(); // Connect to the database using Prisma
     console.log('Connected to the database');
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT ||5000;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
